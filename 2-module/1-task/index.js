f9f85f1f92f7815e6db219340cd38700535359a8
Anna Yaroshenko <yaroshenkoannette@gmail.com>
2021-11-27 15:22:35 +0200

    solution

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sumOfSalaries = 0;
+	for (key in salaries){
+		if (isFinite(salaries[key])) {
+			sumOfSalaries = sumOfSalaries + salaries[key];
+		}
+	}
+	return sumOfSalaries;
 }
