192a8a4315dc63db65c32bdaa930e66ebfd26907
Anna Yaroshenko <yaroshenkoannette@gmail.com>
2021-12-05 12:26:21 +0200

    added module 4

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('ul');
+	friends.forEach(function(item, index){
+		let li = document.createElement('li');
+		li.innerText = item.firstName + " " + item.lastName;
+		ul.appendChild(li);
+	})
+	return ul;
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,22 @@
 function highlight(table) {
-  // ваш код...
+	//add class or attribute depending on value of "Status"
+	let arrOfTdWithStatus= Array.from(document.querySelectorAll('tbody > tr > td:last-child')); 
+	arrOfTdWithStatus.forEach(function(td){
+		if (td.getAttribute('data-available') === 'true'){
+			td.parentNode.classList.add('available');
+		} else if (td.getAttribute('data-available') === 'false'){
+			td.parentNode.classList.add('unavailable');
+		} else { td.parentNode.setAttribute('hidden', true)}
+	})
+ 	// add class depending on value of "Gender" & check if "Age" under 18
+ 	let arrOfTd = Array.from(document.querySelectorAll('td'));
+ 	arrOfTd.forEach(function(td){
+ 		if(td.textContent === 'm'){
+ 			td.parentNode.classList.add('male');
+ 		} else if (td.textContent === 'f'){
+ 			td.parentNode.classList.add('female');
+ 		} else if (td.textContent < 18){
+ 			td.parentNode.style="text-decoration: line-through"
+ 		}	
+ 	})
 }
