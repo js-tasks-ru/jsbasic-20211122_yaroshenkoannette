2189915ae42f5efe134cf831aa794488df8820ee
Anna Yaroshenko <yaroshenkoannette@gmail.com>
2021-12-28 01:06:34 +0200

    add task1 module7

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -10,7 +10,7 @@ export default class RibbonMenu {
   render() {
     this.elem = createElement(`
     <div class="ribbon">
-      <button class="ribbon__arrow ribbon__arrow_left ribbon__arrow_visible">
+      <button class="ribbon__arrow ribbon__arrow_left">
         <img src="/assets/images/icons/angle-icon.svg" alt="icon">
       </button>
   
@@ -18,7 +18,7 @@ export default class RibbonMenu {
        
       </nav>
   
-      <button class="ribbon__arrow ribbon__arrow_right">
+      <button class="ribbon__arrow ribbon__arrow_right ribbon__arrow_visible">
         <img src="/assets/images/icons/angle-icon.svg" alt="icon">
       </button>
     </div>`)
@@ -32,38 +32,52 @@ export default class RibbonMenu {
 
     this.elem.addEventListener('click', this.moveRibbon.bind(this));
     this.ribbonInner.addEventListener('click', this.chooseCategory.bind(this));
-    
+
   }
 
-  moveRibbon(event){
-    if(event.target.closest('.ribbon__arrow_left')){
-      this.ribbonInner.scrollBy(350, 0);
-    } else if( event.target.closest('.ribbon__arrow_right')){
+  moveRibbon(event) {
+    if (event.target.closest('.ribbon__arrow_left')) {
       this.ribbonInner.scrollBy(-350, 0);
+    } else if (event.target.closest('.ribbon__arrow_right')) {
+      this.ribbonInner.scrollBy(350, 0);
     }
 
     this.ribbonInner.addEventListener('scroll', this.showOrHideArrow.bind(this))
   }
 
-  showOrHideArrow(){
+  showOrHideArrow() {
     const arrowLeft = this.elem.querySelector('.ribbon__arrow_left');
     const arrowRight = this.elem.querySelector('.ribbon__arrow_right');
     let scrollLeft = this.ribbonInner.scrollLeft;
     let scrollWidth = this.ribbonInner.scrollWidth;
     let clientWidth = this.ribbonInner.clientWidth;
-    let scrollRight = scrollWidth - scrollLeft - clientWidth; 
-    if(scrollLeft ===0){
-      arrowRight.classList.remove('ribbon__arrow_visible');
-    }else if(scrollRight < 1){
+    let scrollRight = scrollWidth - scrollLeft - clientWidth;
+    if (scrollLeft === 0) {
       arrowLeft.classList.remove('ribbon__arrow_visible');
-    }else{
+    } else if (scrollRight < 1) {
+      arrowRight.classList.remove('ribbon__arrow_visible');
+    } else {
       arrowLeft.classList.add('ribbon__arrow_visible');
       arrowRight.classList.add('ribbon__arrow_visible');
     }
   }
 
-  chooseCategory(event){
+  chooseCategory(event) {
     event.preventDefault();
-   
+    const target = event.target;
+    if (target.tagName != 'A') return;
+    const itemActive = this.ribbonInner.querySelector('.ribbon__item_active');
+    if (itemActive !== null) {
+      itemActive.classList.remove('ribbon__item_active');
+    }
+    target.classList.add('ribbon__item_active');
+
+    let id = event.target.closest("[data-id]").dataset.id;
+    this.elem.dispatchEvent(
+      new CustomEvent('ribbon-select', {
+        detail: id,
+        bubbles: true,
+      })
+    );
   }
 }
